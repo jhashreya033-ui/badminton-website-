3d70863e68b4177b104e290d53c51c11916dd3a8
jhashreya033-ui <jhashreya033@gmail.com>
2026-05-21 17:28:42 +0530

    Add homepage interactions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Badminton World Website Loaded");
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener('click', (event) => {
+    const target = document.querySelector(link.getAttribute('href'));
+
+    if (!target) {
+      return;
+    }
+
+    event.preventDefault();
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+});
